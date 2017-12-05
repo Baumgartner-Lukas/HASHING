fdabacc770967a6bd7d085e72a501bb15e4e9144
WebTree-Baumgartner <baumgartner.webtree@gmail.com>
2017-12-05 20:03:25 +0100

    Added insert method (with contains)
    ToDo:
     - toString()
     - remove

--- src/com/baumgartner/HashSet/ChainingHashSet.java
+++ src/com/baumgartner/HashSet/ChainingHashSet.java
@@ -2,9 +2,7 @@ package com.baumgartner.HashSet;
 
 import com.baumgartner.Interfaces.MyHashSet;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Objects;
+import java.util.Arrays;
 
 public class ChainingHashSet implements MyHashSet {
     private ChainingHashNode[] elements;
@@ -23,11 +21,37 @@ public class ChainingHashSet implements MyHashSet {
 
     @Override
     public boolean insert(Integer key, String data) throws IllegalArgumentException {
-        return false;
+        if (key == null || data.isEmpty()) throw new IllegalArgumentException("Key or data must not be empty");
+        ChainingHashNode node = new ChainingHashNode(key, data);
+        int pos = hashCode(key);
+        if (contains(key)) return false;
+        if (elements[pos] == null) {
+            elements[hashCode(key)] = node;
+        } else {
+            elements[pos].next = node;
+        }
+        return true;
     }
 
+    /**
+     * Checks if key is already in the set.
+     *
+     * @param key
+     * @return returns true if the key is not in the set. False otherwise.
+     * @throws IllegalArgumentException
+     */
     @Override
     public boolean contains(Integer key) throws IllegalArgumentException {
+        if (key == null) throw new IllegalArgumentException("Key must not be null");
+        for (ChainingHashNode n : elements) {
+            while (n != null) {
+                if (key.compareTo(n.key) == 0) {
+                    System.err.println("Key already in set");
+                    return true;
+                }
+                n = n.next;
+            }
+        }
         return false;
     }
 
@@ -38,6 +62,28 @@ public class ChainingHashSet implements MyHashSet {
 
     @Override
     public void clear() {
+    }
+
+    /**
+     * calculates the hashvalue based on the size of the elements array
+     *
+     * @param key key element of key-value pair. Hashvalue
+     * @return returns hashvalue for key
+     */
+    public int hashCode(int key) {
+        return (key % elements.length);
+    }
+
+    @Override
+    public String toString() {
+        StringBuffer buffer = new StringBuffer();
+        for (ChainingHashNode n : elements) {
+            while (n != null) {
+                buffer.append("Key: ").append(n.key).append("Value: ").append(n.data);
+                n = n.next;
+            }
+        }
+        return buffer.toString();
     }
 
     private class ChainingHashNode {
--- src/com/baumgartner/Main.java
+++ src/com/baumgartner/Main.java
@@ -1,8 +1,17 @@
 package com.baumgartner;
 
+import com.baumgartner.HashSet.ChainingHashSet;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        ChainingHashSet set = new ChainingHashSet();
+
+        set.insert(1,"1");
+        set.insert(4,"1");
+        set.insert(15, "3");
+
+        set.toString();
+
     }
 }
